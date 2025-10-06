e58dc97c280b8f6c6d73e6e9a58594377a5932aa
Parthkumar Patel <parthkishan20@gmail.com>
2025-10-05 23:39:20 -0400

    added config for gh action

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), ghPages()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
 })
