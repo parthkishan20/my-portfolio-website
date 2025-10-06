59d0ac56b03978fb1ae2220706d790bfa4cd510b
Parthkumar Patel <parthkishan20@gmail.com>
2025-10-06 00:33:34 -0400

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import Contact from "@/pages/contact"
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
+  <Router basename="/my-portfolio-website/">
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
